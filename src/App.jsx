cce050d92956c3feed47348ad1ee99cc333cf3d5
Raju <rajutamang3456@gmail.com>
2025-05-20 15:53:04 +0545

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
-  const [count, setCount] = useState(0); //initialize
+  const [count, setCount] = useState(10); //initialize
+  const [name, setName] = useState("John "); //initialize
+  const [mode, setMode] = useState("dark");
+  const handleIncrement = () => {
+    setCount(count + 1);
+    setName("Ramesh");
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  let title = "onlinekhabar.com";
 
   return (
     <>
-      <div>
+      <Navbar mode={mode} title={title} />
+      <Banner />
+      <div className="container">
         <h1 className="title">chaitra group</h1>
         <div className="card">
-          <button onClick={() => setCount((count) => count + 5)}>
-            count is {count}
-          </button>
-          <button onClick={() => setCount((count) => count - 5)}>
-            decrease {count}
-          </button>
+          <button onClick={handleIncrement}>Click me to increase</button>
+          <button onClick={handleDecrement}>Click me to decrease</button>
         </div>
         <div>
-          <h4>hello world</h4>
+          <h4>Counter: {count}</h4>
+          <h5>My name is: {name}</h5>
         </div>
       </div>
     </>
