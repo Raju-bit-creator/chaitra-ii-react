e66319d8c8e93bf541cfa422d0f967e13169f498
Raju <rajutamang3456@gmail.com>
2025-06-29 19:56:19 +0545

    update and delete

--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -60,4 +60,49 @@ router.post(
   }
 );
 
+//update product
+
+router.put("/updateproduct/:id", fetchUser, async (req, res) => {
+  const { title, price, description, instock } = req.body;
+
+  try {
+    const newProduct = {};
+    if (title) newProduct.title = title;
+    if (price) newProduct.price = price;
+    if (description) newProduct.description = description;
+    if (instock) newProduct.instock = instock;
+
+    let product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ error: "product not found" });
+    if (!product.user || product.user.toString() !== req.user.id) {
+      return res.status(401).json({ error: "unauthorized access" });
+    }
+    product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: newProduct },
+      { new: true }
+    );
+    res.status(200).json({ product });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+//delete product
+router.delete("/deleteproduct/:id", fetchUser, async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ error: "product not found" });
+    if (!product.user || product.user.toString() !== req.user.id) {
+      return res.status(401).json({ error: "unauthorized access" });
+    }
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "product deleted" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
 module.exports = router;
